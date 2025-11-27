c19c82525cd15dea3ee4af2f56d73c4417d697a7
Václav Doležal <vasikdolezal2006@gmail.com>
2025-11-27 21:49:15 +0100

    celková oprava kolizí, velký pokrok slime už je v pohodě

--- slime.gd
+++ slime.gd
@@ -1,16 +1,13 @@
 extends CharacterBody2D
 
 
-var player 
-
-func _ready():
-	player = get_node("/root/game/tilemap/player")
-
-
+@onready var player = get_node("/root/game/tilemap/player")
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 30.0
 	move_and_slide()
+	
+	
 	
 	
