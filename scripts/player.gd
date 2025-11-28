a50afc7dd18e0408a8a5644308b2e01ac51cf0b9
Václav Doležal <vasikdolezal2006@gmail.com>
2025-11-28 18:33:57 +0100

    staff a cast animace a její základní točení

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,15 +8,15 @@ func _physics_process(delta):
 	character_direction.y = Input.get_axis("move_up", "move_down")
 	
 	#otočení
-	if character_direction.x > 0: %sprite.flip_h = false
-	elif character_direction.y < 0:%sprite.flip_h = true
+	if character_direction.x > 0: %animace.flip_h = false
+	elif character_direction.y < 0:%animace.flip_h = true
 	
 	if character_direction:
 		velocity = character_direction * movement_speed
-		if %sprite.animation != "run": %sprite.animation = "run"
+		if %animace.animation != "run": %animace.animation = "run"
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
-		if %sprite.animation != "idle": %sprite.animation = "idle"
+		if %animace.animation != "idle": %animace.animation = "idle"
 	
 	move_and_slide()
 		
