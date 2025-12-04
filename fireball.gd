0e2dcf5efa79785080b5b77c5f5112179e1533f7
Václav Doležal <vasikdolezal2006@gmail.com>
2025-12-04 22:10:20 +0100

    shooting předělané je to jednoduší a funguje to možná to ještě předělám, další bud dělat životy

--- fireball.gd
+++ fireball.gd
@@ -2,21 +2,36 @@ extends Area2D
 
 var travelled_distance = 0
 
-func _physics_process(delta):
-	const SPEED = 300
-	const RANGE = 1200
+const SPEED: int = 300
+const RANGE = 1200
+
+#func _physics_process(delta):
+#	const SPEED = 300
+#	const RANGE = 1200
 	
-	var direction = Vector2.RIGHT.rotated(rotation)
-	position += direction * SPEED * delta
+func _process(delta: float) -> void:
+	position += transform.x * SPEED * delta
 	
-	travelled_distance += SPEED * delta
-	if travelled_distance > RANGE:
-		queue_free()
+
+
+#	var direction = Vector2.RIGHT.rotated(rotation)
+#	position += direction * SPEED * delta
+	
+	
+		
+	
+#	travelled_distance += SPEED * delta 
+#	if travelled_distance > RANGE:
+#		queue_free()
 		
 		
 
 
-func _on_body_entered(body):
+#func _on_body_entered(body):
+#	queue_free()
+#	if body.has_method("take_damage"):
+#		body.take_damage()"
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
 	queue_free()
-	if body.has_method("take_damage"):
-		body.take_damage()
