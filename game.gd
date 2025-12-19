c50fd21edb46bc2433739585e42dd62e5ad7d5f5
Václav Doležal <vasikdolezal2006@gmail.com>
2025-12-19 17:01:47 +0100

    slimové se ukážou až tehdy kdy jsou na mapě, stále je třeba opravit sloupy je to rozdělané

--- game.gd
+++ game.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var path_follow = $Path2D/PathFollow2D
 var MobScene = preload("res://slime.tscn")
 
+# globální floor oblast (Rect2)
+@export var floor_rect := Rect2(Vector2(0, 0), Vector2(1154.0, 656.0))
+
 func _ready():
 	randomize()
 	for i in range(5):
@@ -12,5 +15,8 @@ func spaw_mob():
 	var mob = MobScene.instantiate()
 	path_follow.progress_ratio = randf()
 	mob.global_position = path_follow.global_position
-	mob.z_index = 10
+	
+	# přidej floor_rect do mobu
+	mob.floor_rect = floor_rect
+	
 	add_child(mob)
--- slime.gd
+++ slime.gd
@@ -1,30 +1,35 @@
 extends CharacterBody2D
 
+var floor_rect := Rect2()  # bude nastaven při spawnu
 var health = 3
 
-@onready var player = get_node("/root/game/player")
 @onready var an = $animace
+@onready var player = get_node("/root/game/player")
 
-func _ready() -> void:
+func _ready():
+	add_to_group("slimes")
+	visible = false  # začíná skrytý
 	an.play("move")
+
+#tohle se stará o vidilenost 
+func _process(_delta):
+	if floor_rect.has_point(global_position):
+		visible = true
+	else:
+		visible = false
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 30.0
 	move_and_slide()
-	
+
 func take_damage():
 	health -= 1
 	an.play("hurt")
-	await an.animation_finished # počká se 3 sekundy a pak se pustí animaci move
+	await an.animation_finished
 	an.play("move")
 	
 	if health == 0:
 		an.play("smoke")
-		await an.animation_finished # bez tohoto to akčuali nejde
+		await an.animation_finished
 		queue_free()
-		
-		
-		
-		
-		
