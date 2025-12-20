8000f6684bef0afd1591f9740d7632030151a110
Václav Doležal <vasikdolezal2006@gmail.com>
2025-12-20 16:40:18 +0100

    nekonečné spawnování podle timeru, oprava kolizí a damgae kolizíí

--- game.gd
+++ game.gd
@@ -1,22 +1,25 @@
 extends Node2D
 
 @onready var path_follow = $Path2D/PathFollow2D
+@onready var timer = $Timer
+
 var MobScene = preload("res://slime.tscn")
 
-# globální floor oblast (Rect2)
-@export var floor_rect := Rect2(Vector2(0, 0), Vector2(1154.0, 656.0))
+# globální floor oblast (musí sedět s mapou)
+@export var floor_rect := Rect2(Vector2(0, 0), Vector2(1154, 656))
 
 func _ready():
 	randomize()
-	for i in range(5):
-		spaw_mob()
+	timer.start()
 
-func spaw_mob():
+func spawn_mob():
 	var mob = MobScene.instantiate()
 	path_follow.progress_ratio = randf()
 	mob.global_position = path_follow.global_position
-	
-	# přidej floor_rect do mobu
-	mob.floor_rect = floor_rect
-	
+
+	mob.floor_rect = floor_rect # předání inforamce a mapě slimům
+
 	add_child(mob)
+
+func _on_timer_timeout() -> void:
+	spawn_mob()
