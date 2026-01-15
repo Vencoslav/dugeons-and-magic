7ec64af33a233c358068253ed49243db57709bcd
Václav Doležal <vasikdolezal2006@gmail.com>
2026-01-15 22:17:15 +0100

    jednoduché pause menu, bez main menu se bude připravovat

--- game.gd
+++ game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 @onready var path_follow = %PathFollow2D
 @onready var timer = $GlobalTimer
 @onready var healthTimer = $IncreseHealthTimer
@@ -11,6 +12,20 @@ var slimeSpeedBonus := 0
 var maxSlimes := 5
 var currentSlimes := 0
 
+var is_paused := false
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	$PauseMenu.process_mode = Node.PROCESS_MODE_ALWAYS
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("pause_menu"):
+		is_paused = true
+		get_tree().paused = true
+		$PauseMenu.visible = true
+
+		
 func spawn_mob():
 	if currentSlimes >= maxSlimes:
 		return
@@ -39,10 +54,9 @@ func _on_player_health_depleted() -> void:
 	%GameOver.visible = true
 	get_tree().paused = true
 
-func _on_button_exit_pressed() -> void:
-	get_tree().quit()
 
 func _on_button_try_again_pressed() -> void:
+	Input.flush_buffered_events()
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
@@ -51,6 +65,25 @@ func _on_increse_health_timer_timeout() -> void:
 	slimeSpeedBonus += 5.0
 	maxSlimes += 2
 	player.damageRate += 5.0
+
+func _on_button_quit_game_pressed() -> void:
+	Input.flush_buffered_events() # odstraní předochí zobrazení zakliknutí tlačítka	
+	get_tree().quit()
 	
+
+func _on_button_restart_pressed() -> void:
+	Input.flush_buffered_events()
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func _on_button_resume_pressed() -> void:
+	Input.flush_buffered_events()
+	is_paused = false
+	get_tree().paused = false
+	$PauseMenu.visible = false
+	
+
+
+
+
 	#dodělej text když se zvíší obtížnost
-	#udělej lepší kolize
