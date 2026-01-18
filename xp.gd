e2e7f5ac063c22afc6495013e86d11c7c93f6d79
Václav Doležal <vasikdolezal2006@gmail.com>
2026-01-18 19:43:35 +0100

    překopání xp baru

--- xp.gd
+++ xp.gd
@@ -1,11 +1,9 @@
-extends Area2D
+extends Pickups
+class_name Xp
 
+@export var XP : float
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func activate():
+	super.activate()
+	prints("+" +str(XP) + "XP")
+	player_reference.gain_XP(XP)
